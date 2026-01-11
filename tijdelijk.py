d149b7c17adb04620d39177c2bbaf1d0138867fc
RishiRakhan <rishirakhan@hotmail.com>
2026-01-11 22:43:34 +0100

    bestand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,25 @@
+prijzen = {
+    "aardbei" : 3,
+    "vanille" : 4,
+    "chocolade" : 5
+}
+
+aanbieding = prijzen["aardbei"] * 8
+
+reclame_tekst = f"vandaag in de aanbeiding: vanille-ijs, 1 liter - slechts € {aanbieding} "
+
+reclame_tekst2 = reclame_tekst[:62]
+
+reclame_tekst3 = reclame_tekst2.upper()
+
+reclame_tekst4 = reclame_tekst3.split()
+
+for el in reclame_tekst4:
+    if len(el) >= 5:
+        print(el.upper())
+    else:
+        print(el.lower())           
+
+print(reclame_tekst4)
+
+
